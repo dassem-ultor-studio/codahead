f67368fb67ec1bfb480e82e87ed5667c1bf63f72
dassem-ultor <dassem.ultor@o2.pl>
2019-02-23 16:27:50 +0100

    Create initial interactor implementation

---
+++ app/src/main/java/news/ahead/cod/myapplication/main/NewsInteractor.kt
@@ -0,0 +1,29 @@
+package news.ahead.cod.myapplication.main
+
+import news.ahead.cod.myapplication.model.ResponseModel
+import news.ahead.cod.myapplication.network.APIClient
+import news.ahead.cod.myapplication.network.NewsService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class NewsInteractor : MainContract.MainInteractor {
+
+    override fun latest(callback: MainContract.MainInteractor.ResponseCallback) {
+        val retrofit = APIClient.client() ?: return
+
+        val service = retrofit.create(NewsService::class.java)
+
+        service.latestNews().enqueue(object : Callback<ResponseModel> {
+            override fun onResponse(call: Call<ResponseModel>, response: Response<ResponseModel>) {
+                val articles = response.body()?.articles ?: return
+
+                callback.onResponse(articles)
+            }
+
+            override fun onFailure(call: Call<ResponseModel>, error: Throwable) {
+                callback.onError(error)
+            }
+        })
+    }
+}
