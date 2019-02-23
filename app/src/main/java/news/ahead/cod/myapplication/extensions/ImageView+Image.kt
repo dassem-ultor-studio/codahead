5b6ace34c67c7918c2deb1c3b7d9f810bcd60f04
dassem-ultor <dassem.ultor@o2.pl>
2019-02-23 17:25:33 +0100

    Add ImageView extension to load image

---
+++ app/src/main/java/news/ahead/cod/myapplication/extensions/ImageView+Image.kt
@@ -0,0 +1,15 @@
+package news.ahead.cod.myapplication.extensions
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
+import news.ahead.cod.myapplication.R
+
+fun ImageView.loadImageRoundedCorners(url: String) {
+    Glide.with(this)
+            .load(url)
+            .transform(CenterCrop(), RoundedCornersTransformation(12, 0))
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(this)
+}
