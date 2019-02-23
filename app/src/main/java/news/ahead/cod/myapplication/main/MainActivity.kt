99d5cc84ae95590a9d6f6c8a0fccbe60d0358491
dassem-ultor <dassem.ultor@o2.pl>
2019-02-23 16:10:47 +0100

    Create initial MainView implementation

--- app/src/main/java/news/ahead/cod/myapplication/main/MainActivity.kt
+++ app/src/main/java/news/ahead/cod/myapplication/main/MainActivity.kt
@@ -1,13 +1,31 @@
 package news.ahead.cod.myapplication.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import news.ahead.cod.myapplication.R
+import news.ahead.cod.myapplication.adapter.NewsAdapter
+import news.ahead.cod.myapplication.extensions.setVisibility
+import news.ahead.cod.myapplication.model.Article
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), MainContract.MainView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    override fun toggleProgress(shouldShowProgress: Boolean) {
+        mainActivity_progressBar.setVisibility(shouldShowProgress)
+    }
+
+    override fun updateList(articles: List<Article>) {
+        mainActivity_recyclerView.adapter = NewsAdapter(articles) {}
+    }
+
+    override fun onError(error: Throwable) {
+        Toast.makeText(this, error.localizedMessage, Toast.LENGTH_SHORT).show()
+    }
+
 }
