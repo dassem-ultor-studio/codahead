7333cbe53c6f7af3e1cac90690ae086f76577d87
dassem-ultor <dassem.ultor@o2.pl>
2019-02-23 15:56:43 +0100

    Create simple article adapter

---
+++ app/src/main/java/news/ahead/cod/myapplication/adapter/NewsAdapter.kt
@@ -0,0 +1,26 @@
+package news.ahead.cod.myapplication.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.news_view_holder.view.*
+import news.ahead.cod.myapplication.R
+import news.ahead.cod.myapplication.model.Article
+
+class NewsAdapter(val list: List<Article>, val itemClickHandler: (Article) -> Unit) :
+        RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.news_view_holder, parent, false)
+        return NewsViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        holder.itemView.newsViewHolder_title.text = list[position].title
+    }
+
+    class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
