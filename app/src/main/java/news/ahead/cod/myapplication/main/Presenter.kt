7f5153006a82c7ae15ab8092465111fc89b57c54
dassem-ultor <dassem.ultor@o2.pl>
2019-02-23 16:36:30 +0100

    Create initial presenter implementation

---
+++ app/src/main/java/news/ahead/cod/myapplication/main/Presenter.kt
@@ -0,0 +1,33 @@
+package news.ahead.cod.myapplication.main
+
+import news.ahead.cod.myapplication.model.Article
+
+class Presenter(var view: MainContract.View?, var interactor: MainContract.Interactor?) :
+        MainContract.Presenter, MainContract.Interactor.ResponseCallback {
+
+    override fun onRefresh() {
+        view?.toggleProgress(true)
+    }
+
+    override fun requestData() {
+        interactor?.latest(this)
+    }
+
+    override fun onDestroy() {
+        view = null
+    }
+
+    override fun onResponse(articles: List<Article>) {
+        val view = view ?: return
+
+        view.updateList(articles)
+        view.toggleProgress(false)
+    }
+
+    override fun onError(error: Throwable) {
+        val view = view ?: return
+
+        view.onError(error)
+        view.toggleProgress(false)
+    }
+}
