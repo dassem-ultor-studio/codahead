dce30e0667b29fba9ed4dd28c1781d4d25ed53eb
dassem-ultor <dassem.ultor@o2.pl>
2019-02-24 11:59:40 +0100

    Adjust NewsService
    Change endpoint to one that allows paging, add paging parameters

--- app/src/main/java/news/ahead/cod/myapplication/network/NewsService.kt
+++ app/src/main/java/news/ahead/cod/myapplication/network/NewsService.kt
@@ -1,14 +1,16 @@
 package news.ahead.cod.myapplication.network
 
+import news.ahead.cod.myapplication.main.PagingInfo
 import news.ahead.cod.myapplication.model.ResponseModel
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsService {
-    @GET("top-headlines")
-    fun latestNews(@Query("country") country: String = "us",
-                   @Query("category") category: String = "business",
+    @GET("everything")
+    fun latestNews(@Query("q") category: String = "bitcoin",
+                   @Query("pageSize") pageSize: Int = PagingInfo.pageSize,
+                   @Query("page") page: Int = 1,
                    @Query("apiKey") apiKey: String = "332900cd194a4d998acf71f4d85aa830"):
             Call<ResponseModel>
 }
